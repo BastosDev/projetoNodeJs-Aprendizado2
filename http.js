04d21bc380c97559cea845e9f7d75eef75501ff8
BastosDev <gustavoporto.gp63@gmail.com>
2024-12-29 12:11:26 -0300

    add new code line

--- http.js
+++ http.js
@@ -1,12 +1,43 @@
 const http = require("node:http")
 
-const server = http.createServer((request, response) => {
+const server = http.createServer( async (request, response) => {
     const { method, statusCode, url } = request
 
+    const bodyPromise = new Promise((resolve, reject) => {
+        let body 
+        request.on("data", () => {  
+            body = JSON.parse(data)
+
+        })
+        request.on("end" , data  => {
+            resolve(body)
+        })
+    })
+
+    const sports = ["soccer", "volley","basketball", "tennis"]
+
     if (url === "/") {
-        response.write("Hello from node")
+        response.write("<div><h1> Hello from node </h1> <p> http server <\p> </div>")
         response.end()
+        return
     }
+
+    if (url === "/api/sports") {
+        if (method === "GET") {
+        response.write(JSON.stringify(sports))
+        response.end()
+        return
+        }
+
+        if (method === "POST") {
+            const body = await bodyPromise
+            console.log(body)
+        }
+    }
+
+    response.statusCode = 404
+    response.write("Pagina não encontrada")
+    response.end()
 })
 
 

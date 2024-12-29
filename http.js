b9a892e3009e24a840d1b76ecb88edcce4a99491
BastosDev <gustavoporto.gp63@gmail.com>
2024-12-28 21:37:13 -0300

    add new code line

--- http.js
+++ http.js
@@ -1,5 +1,17 @@
 const http = require("node:http")
+const port = 6000
 
-http.createServer((request, response) => {
-    
+const server = http.createServer((request, response) => {
+    const { method, statusCode, url } = request
+
+    if (url === "/") {
+        return response.write("Hello from node")
+    }
+})
+
+
+server.listen(port, "localhost", () => {
+
+    console.log(`server running on https://localhost:/${port}`)
+
 })
